76adcb08517785cb2569dcb4712a890d8387aa9b
Cathy Nguyen <cathy.nguyen.75013@gmail.com>
2024-06-17 18:15:55 +0200

    fix: add checks to know if the file exists

--- ping/src/main/java/fr/epita/assistants/myide/domain/entity/git/GitAdd.java
+++ ping/src/main/java/fr/epita/assistants/myide/domain/entity/git/GitAdd.java
@@ -15,6 +15,9 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.Arrays;
 
 public class GitAdd implements Feature {
