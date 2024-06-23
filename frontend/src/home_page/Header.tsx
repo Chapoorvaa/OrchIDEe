0249a16c19b0832b2a81a1870358001719ab0437
Abigaëlle Panhelleux <abigaelle.panhelleux@epita.fr>
2024-06-23 15:55:56 +0200

    feat(homepage): relocated all the components and made the new button make the center cube disappear

---
+++ frontend/src/home_page/Header.tsx
@@ -0,0 +1,11 @@
+const Header: React.FC = () => {
+    return (
+        <div className="fixed insert-x-0 top-0 bg-gray-800">
+            <header className="text-gray-100 border-b-2 border-gray-600 text-center w-screen px-4 py-2">
+                Welcome to OrchIDEe
+            </header>
+        </div>
+    );
+}
+
+export default Header;
