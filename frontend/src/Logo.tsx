7a724df92952825008c4aef4ed7f370ca077c8b4
Abigaëlle Panhelleux <abigaelle.panhelleux@epita.fr>
2024-06-23 14:46:46 +0200

    feat(home-page): added the full layout of the home page

---
+++ frontend/src/Logo.tsx
@@ -0,0 +1,9 @@
+const Logo: React.FC = () => {
+    return (
+        <div className="mx-auto size-4/5">
+            <img src="logo.png" alt="OrchIDEe Logo"/>
+        </div>
+    );
+}
+
+export default Logo;
