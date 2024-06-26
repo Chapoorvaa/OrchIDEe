2e6ff3a5fb9809331f4f9d2f0be5b5df7b4c0488
Julie Ducastel <julie.ducastel@epita.fr>
2024-06-26 13:46:06 +0200

    feat: it works

--- frontend/src/BasePage.tsx
+++ frontend/src/BasePage.tsx
@@ -1,13 +1,14 @@
 import React from 'react';
 import Chatbot from './chatbot/Chatbot';
+import GitStatus from './components/GitStatus';
 
 export const HomePage: React.FC = () => {
   return (
     <>
-      <div className="m-0 grid h-screen w-screen grid-cols-[50px_300px_1fr_300px_50px] grid-rows-[50px_1fr_repeat(2,50px)] gap-2">
+      <div className="m-0 grid h-screen w-screen grid-cols-[50px_300px_1fr_300px_50px] grid-rows-[50px_1fr_repeat(2,50px)] gap-2 bg-gray-600">
         <div className="col-span-5">Status bar</div>
         <div className="col-start-1 row-start-2">Tree and Git buttons</div>
-        <div className="col-start-2 row-start-2">Git or Tree</div>
+        <div className="col-start-2 row-start-2"><GitStatus/></div>
         <div className="col-start-3 row-start-2">Editor</div>
         <div className="col-start-4 row-start-2"><Chatbot/></div>
         <div className="col-start-5 row-start-2">Bot button</div>
