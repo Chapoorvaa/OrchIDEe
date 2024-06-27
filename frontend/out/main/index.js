91dc1c4907bd3b65e626fa344c5e6141e0da27b2
Cathy Nguyen <cathy.nguyen.75013@gmail.com>
2024-06-27 02:50:44 +0200

    move to electron config

---
+++ frontend/out/main/index.js
@@ -0,0 +1,69 @@
+"use strict";
+const electron = require("electron");
+const path = require("path");
+const utils = require("@electron-toolkit/utils");
+const remoteMain = require("@electron/remote/main");
+function _interopNamespaceDefault(e) {
+  const n = Object.create(null, { [Symbol.toStringTag]: { value: "Module" } });
+  if (e) {
+    for (const k in e) {
+      if (k !== "default") {
+        const d = Object.getOwnPropertyDescriptor(e, k);
+        Object.defineProperty(n, k, d.get ? d : {
+          enumerable: true,
+          get: () => e[k]
+        });
+      }
+    }
+  }
+  n.default = e;
+  return Object.freeze(n);
+}
+const remoteMain__namespace = /* @__PURE__ */ _interopNamespaceDefault(remoteMain);
+const icon = path.join(__dirname, "../../resources/icon.png");
+function createWindow() {
+  remoteMain__namespace.initialize();
+  const mainWindow = new electron.BrowserWindow({
+    width: 900,
+    height: 670,
+    show: false,
+    icon: path.join(__dirname, "../../ressources/icon.png"),
+    autoHideMenuBar: true,
+    ...process.platform === "linux" ? { icon } : {},
+    webPreferences: {
+      preload: path.join(__dirname, "../preload/index.js"),
+      sandbox: false,
+      nodeIntegration: true,
+      contextIsolation: false
+    }
+  });
+  mainWindow.on("ready-to-show", () => {
+    mainWindow.show();
+  });
+  mainWindow.webContents.setWindowOpenHandler((details) => {
+    electron.shell.openExternal(details.url);
+    return { action: "deny" };
+  });
+  if (utils.is.dev && process.env["ELECTRON_RENDERER_URL"]) {
+    mainWindow.loadURL(process.env["ELECTRON_RENDERER_URL"]);
+  } else {
+    mainWindow.loadFile(path.join(__dirname, "../renderer/index.html"));
+  }
+  remoteMain__namespace.enable(mainWindow.webContents);
+}
+electron.app.whenReady().then(() => {
+  utils.electronApp.setAppUserModelId("com.electron");
+  electron.app.on("browser-window-created", (_, window) => {
+    utils.optimizer.watchWindowShortcuts(window);
+  });
+  electron.ipcMain.on("ping", () => console.log("pong"));
+  createWindow();
+  electron.app.on("activate", function() {
+    if (electron.BrowserWindow.getAllWindows().length === 0) createWindow();
+  });
+});
+electron.app.on("window-all-closed", () => {
+  if (process.platform !== "darwin") {
+    electron.app.quit();
+  }
+});
--- frontend/src/renderer/src/BasePage.tsx
+++ frontend/src/renderer/src/BasePage.tsx
@@ -2,6 +2,21 @@ import React from 'react';
 import Chatbot from './chatbot/Chatbot';
 
 export const HomePage: React.FC = () => {
+  const openFileDialog = async () => {
+    const options = {
+      title: 'Open a file',
+      buttonLabel: 'Select',
+      properties: ['openFile', 'multiSelections']
+    };
+
+    const {dialog} = require("@electron/remote");
+    await dialog.showOpenDialog({options}).
+    then(async (result) => {
+      console.log(result.filePaths + " a ete choisit !")
+    }).catch((err) => {
+      console.log(err)
+    })
+   };
   return (
     <>
       <div className="m-0 grid h-screen w-screen grid-cols-[50px_300px_1fr_300px_50px] grid-rows-[50px_1fr_repeat(2,50px)] gap-2">
@@ -13,6 +28,9 @@ export const HomePage: React.FC = () => {
         <div className="col-start-5 row-start-2">Bot button</div>
         <div className="col-span-5 col-start-1 row-start-3">Terminal</div>
         <div className="col-span-5 row-start-4">Bottom bar</div>
+        <div className="col-start-1 row-start-5">
+          <button onClick={openFileDialog}>Open File Dialog</button>
+        </div>
       </div>
     </>
   );
