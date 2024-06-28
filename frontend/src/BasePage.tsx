3607e3520ce78e229d2b6cc229e3cd27139f1143
Cathy Nguyen <cathy.nguyen.75013@gmail.com>
2024-06-28 18:20:48 +0200

    feat: code editor

--- frontend/src/BasePage.tsx
+++ frontend/src/BasePage.tsx
@@ -1,30 +1,32 @@
-import React, { useState } from 'react';
-import Chatbot from './chatbot/Chatbot';
-import Git from './git/Git';
-import LeftBar from './leftBar/LeftBar';
-import FileTree from './fileTree/fileTree';
-import RightBar from './rightBar/rightBar';
+import React, { useState } from "react";
+import Chatbot from "./chatbot/Chatbot";
+import Git from "./git/Git";
+import LeftBar from "./leftBar/LeftBar";
+import FileTree from "./fileTree/fileTree";
+import RightBar from "./rightBar/rightBar";
+import CodeEditor from "./codeEditor/CodeEditor";
 
 export const BasePage: React.FC = () => {
-
   const [visibleComponent, setVisibleComponent] = useState<string>("");
   const [visibleComponent2, setVisibleComponent2] = useState<string>("");
 
   const openFileDialog = async () => {
     const options = {
-      title: 'Open a file',
-      buttonLabel: 'Select',
-      properties: ['openFile', 'multiSelections']
+      title: "Open a file",
+      buttonLabel: "Select",
+      properties: ["openFile", "multiSelections"],
     };
 
-    const {dialog} = require("@electron/remote");
-    await dialog.showOpenDialog({options})
-      .then(async (result: { filePaths: string; }) => {
-        console.log(result.filePaths + " was picked!")
-      }).catch((err: any) => {
-        console.log(err)
+    const { dialog } = require("@electron/remote");
+    await dialog
+      .showOpenDialog({ options })
+      .then(async (result: { filePaths: string }) => {
+        console.log(result.filePaths + " was picked!");
+      })
+      .catch((err: any) => {
+        console.log(err);
       });
-   };
+  };
 
   const handleShowFileTree = () => {
     if (visibleComponent === "fileTree") {
@@ -54,30 +56,33 @@ export const BasePage: React.FC = () => {
     <div className="m-0 grid h-screen w-screen grid-cols-[50px_300px_1fr_300px_50px] grid-rows-[50px_1fr_repeat(2,50px)] bg-gray-700 text-gray-100">
       <div className="col-span-5">Status bar</div>
       <div className="col-start-1 row-start-2">
-        <LeftBar 
-          onShowFileTree={handleShowFileTree} 
-          onShowGitInterface={handleShowGitInterface} 
+        <LeftBar
+          onShowFileTree={handleShowFileTree}
+          onShowGitInterface={handleShowGitInterface}
         />
       </div>
       <div className="col-start-2 row-start-2">
         {visibleComponent === "fileTree" && <FileTree />}
         {visibleComponent === "gitInterface" && <Git />}
       </div>
-      <div className="col-start-3 row-start-2">Editor</div>
-      <div className="col-start-5 row-start-2">
-        <RightBar 
-          onShowBot={handleShowBot} 
+      <div className="col-start-3 row-start-2">
+        <CodeEditor
+          language="java"
+          tabSize={4}
+          opened={["pagec kd;kl;kl;k1", "pagecdlkl;kcl;2", "page 3"]}
         />
       </div>
+      <div className="col-start-5 row-start-2">
+        <RightBar onShowBot={handleShowBot} />
+      </div>
       <div className="col-start-4 row-start-2">
-        {visibleComponent2 === "chatBot" && <Chatbot/>}
+        {visibleComponent2 === "chatBot" && <Chatbot />}
       </div>
       <div className="col-span-5 col-start-1 row-start-3">Terminal</div>
       <div className="col-span-5 row-start-4">Bottom bar</div>
-      <div className="col-start-1 row-start-5">
-      </div>
+      <div className="col-start-1 row-start-5"></div>
     </div>
   );
-}
+};
 
 export default BasePage;
--- frontend/src/codeEditor/CodeEditor.tsx
+++ frontend/src/codeEditor/CodeEditor.tsx
@@ -1,54 +1,77 @@
 import { Editor, loader, type Monaco } from "@monaco-editor/react";
 import Monokai_Bright from "./editor-theme/Monokai.json";
-import React from "react";
 import path from "path";
+import { useState } from "react";
 
-function ensureFirstBackSlash(str) {
-    return str.length > 0 && str.charAt(0) !== '/' ? '/' + str : str;
-  }
-  
-  function uriFromPath(_path) {
-    const pathName = path.resolve(_path).replace(/\\/g, '/');
-    return encodeURI('file://' + ensureFirstBackSlash(pathName));
-  }
-  
-  loader.config({
-    paths: {
-      vs: "/home/kty/Desktop/OrchIDEe/frontend/node_modules/monaco-editor/min/vs",
-    },
-  });
+const __editor = path.resolve("node_modules/monaco-editor/min/vs");
+loader.config({
+  paths: {
+    vs: __editor,
+  },
+});
 
 interface Config {
   language: string;
   tabSize: number;
+  opened: string[];
 }
 
 // TODO: load the theme that in the config file
 
 const CodeEditor = (props: Config) => {
-    console.log(__dirname)
-  const options = {
-    tabSize: props.tabSize,
-  };
+  const [currentPage, setCurrentPage] = useState(0);
 
-  const handleEditorDidMount = (monaco: Monaco) => {
-    monaco.editor.defineTheme("MonokaiBright", {
-      base: "vs",
-      ...Monokai_Bright}
+  if (props.opened.length != 0) {
+    const options = {
+      tabSize: props.tabSize,
+    };
+
+    const handleEditorDidMount = (monaco: Monaco) => {
+      monaco.editor.defineTheme("MonokaiBright", {
+        base: "vs",
+        ...Monokai_Bright,
+      });
+    };
+
+    const handleChangePage = (index: number) => {
+      setCurrentPage(index);
+      console.log(currentPage);
+    };
+
+    return (
+      <>
+        <div className="block ">
+          <ul className="flex justify-start overflow-x-scroll no-scrollbar">
+            {props.opened && props.opened.length > 0 ? (
+              props.opened.map((content) => (
+                <li
+                  className=" bg-slate-500 px-10"
+                  key={props.opened.indexOf(content)}
+                  onClick={() =>
+                    handleChangePage(props.opened.indexOf(content))
+                  }
+                >
+                  {content}
+                </li>
+              ))
+            ) : (
+              <li></li>
+            )}
+          </ul>
+        </div>
+        <Editor
+          height="90vh"
+          width="100%"
+          theme="MonokaiBright"
+          defaultLanguage={props.language}
+          value={props.opened[currentPage]}
+          defaultValue={props.opened[currentPage]}
+          options={options}
+          beforeMount={handleEditorDidMount}
+        />
+      </>
     );
-  };
-
-  return (
-    <Editor
-      height="90vh"
-      width="100%"
-      theme="MonokaiBright"
-      defaultLanguage={props.language}
-      defaultValue="// idk "
-      options={options}
-      beforeMount={handleEditorDidMount}
-    />
-  );
+  }
 };
 
 export default CodeEditor;
