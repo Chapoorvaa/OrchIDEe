37687ed16a088fb5d42de969b955d0f0e6427ca0
Abigaëlle Panhelleux <abigaelle.panhelleux@epita.fr>
2024-06-28 19:49:38 +0200

    fix(logo): fixed the size of the Logo

--- frontend/src/home_page/Logo.tsx
+++ frontend/src/home_page/Logo.tsx
@@ -1,6 +1,6 @@
 const Logo: React.FC = () => {
     return (
-        <div className="mx-auto size-4/5">
+        <div className="mx-auto mb-4 size-2/3">
             <img src="../src/assets/icon.png" alt="OrchIDEe Logo"/>
         </div>
     );
