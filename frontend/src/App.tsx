99703da1d84568b6c80d244ecd286362b4be5954
Emmanuel Vouillon <emmanuel.vouillon@epita.fr>
2024-06-29 23:29:21 +0200

    feat: adds a config file to read from

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,7 @@
-import { BasePage } from './BasePage'
+import { BasePage } from "./BasePage";
 import HomePage from "./home_page/HomePage";
 import { useState } from "react";
+import projectConfig from "../config.json";
 
 export interface ProjectDescProps {
   name: string;
@@ -14,25 +15,33 @@ function App() {
   const [language, setLanguage] = useState("");
   const [basepage, setBasePage] = useState(false);
 
+  console.log(projectConfig.theme);
+  console.log(projectConfig.font);
+  console.log(projectConfig.fontSize);
+  console.log(projectConfig.spacing);
+
   const desc: ProjectDescProps = {
     name,
     path,
-    language
+    language,
   };
 
   return (
     <div>
-        { !basepage && <HomePage
-            projectName={name}
-            path={path}
-            language={language}
-            setName={setName}
-            setPath={setPath}
-            setLanguage={setLanguage}
-            setBasePage={setBasePage}/> }
-        { basepage && <BasePage {...desc} />}
+      {!basepage && (
+        <HomePage
+          projectName={name}
+          path={path}
+          language={language}
+          setName={setName}
+          setPath={setPath}
+          setLanguage={setLanguage}
+          setBasePage={setBasePage}
+        />
+      )}
+      {basepage && <BasePage {...desc} />}
     </div>
   );
 }
 
-export default App
+export default App;
