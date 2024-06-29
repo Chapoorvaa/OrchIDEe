464c115a0f0534a1949184747afe5345838a72be
Emmanuel Vouillon <emmanuel.vouillon@epita.fr>
2024-06-29 23:10:58 +0200

    feat(back-chatbot): adds some context between requests

--- ping/src/main/java/fr/epita/assistants/myide/domain/entity/any/AnyChatbot.java
+++ ping/src/main/java/fr/epita/assistants/myide/domain/entity/any/AnyChatbot.java
@@ -1,8 +1,7 @@
 package fr.epita.assistants.myide.domain.entity.any;
 
-import com.fasterxml.jackson.databind.JsonNode;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import com.mashape.unirest.http.HttpResponse;
+import com.mashape.unirest.http.JsonNode;
 import com.mashape.unirest.http.Unirest;
 import fr.epita.assistants.myide.domain.entity.ExtraFeatures;
 import fr.epita.assistants.myide.domain.entity.Feature;
@@ -10,42 +9,31 @@ import fr.epita.assistants.myide.domain.entity.Project;
 import fr.epita.assistants.myide.domain.entity.report.ChatbotFeatureReport;
 import fr.epita.assistants.myide.utils.Logger;
 
-import java.io.BufferedReader;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-
 public class AnyChatbot implements Feature {
 
     @Override
     public ExecutionReport execute(Project project, Object... params) {
         try {
+            AnyAspect aspect = (AnyAspect) project.getAspects().stream().filter(e -> e instanceof AnyAspect).findFirst().orElse(null);
+
             String model = params[0].toString();
             String prompt = params[1].toString();
             int contextLength = 4096;
-            HttpResponse<InputStream> response
+            HttpResponse<JsonNode> response
                     = Unirest.post("http://localhost:11434/api/generate")
                     .body("{\"model\":\"" + model + "\"," +
                             "\"prompt\":\"" + prompt + "\"," +
-                            " \"options\":{\"num_ctx\":" + contextLength + "}" +
+                            " \"options\":{\"num_ctx\":" + contextLength + "}," +
+                            "\"stream\": false," +
+                            "\"context\": " + aspect.context + "" +
                             "}")
-                    .asBinary();
+                    .asJson();
 
-            InputStream inputStream = response.getBody();
-            BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream));
-            ObjectMapper objectMapper = new ObjectMapper();
-            StringBuilder wholeResponse = new StringBuilder();
+            aspect.context = response.getBody().getObject().get("context").toString();
 
-            String line;
-            while ((line = reader.readLine()) != null) {
-                JsonNode jsonNode = objectMapper.readTree(line);
-
-                wholeResponse.append(jsonNode.get("response").asText());
-            }
-
-            reader.close();
-
-            return new ChatbotFeatureReport(wholeResponse.toString(), true);
-        } catch (Exception ignored) {
+            return new ChatbotFeatureReport(response.getBody().getObject().getString("response"), true);
+        } catch (Exception e) {
+            Logger.logError("An error occured chatbotting: " + e.getMessage());
             return new ChatbotFeatureReport("", false);
         }
     }
