41f51cae072b30f00809d9fe95c83e77bc1116f9
Abigaëlle Panhelleux <abigaelle.panhelleux@epita.fr>
2024-06-30 21:15:37 +0200

    fix(homepage): fixed the open projet with a non java or cpp project

--- frontend/src/home_page/CenterCube.tsx
+++ frontend/src/home_page/CenterCube.tsx
@@ -10,37 +10,17 @@ interface CenterCubeProps {
   setLanguage: React.Dispatch<React.SetStateAction<string>>;
 }
 
-const CenterCube: React.FC<CenterCubeProps> = ({
-  setBasePage,
-  setHomepage,
-  setName,
-  setPath,
-  setLanguage,
-}) => {
-  const [error, setError] = useState(false);
-  return (
-    <div className="flex flex-col justify-center text-center m-auto drop-shadow-lg rounded-xl border-2 border-skin-stroke-light bg-skin-bg-dark h-1/2 w-1/4">
-      <Logo />
-      {error && (
-        <p className="text-red-500">The directory selected is not a project</p>
-      )}
-      <div className="flex justify-around">
-        <Button
-          label="new"
-          setBasePage={setBasePage}
-          setFunction={setHomepage}
-        />
-        <Button
-          setBasePage={setBasePage}
-          label="open"
-          setFunction={setError}
-          setName={setName}
-          setPath={setPath}
-          setLanguage={setLanguage}
-        />
-      </div>
-    </div>
-  );
-};
+const CenterCube: React.FC<CenterCubeProps> = ({ setBasePage, setHomepage, setName, setPath, setLanguage }) => {
+    const [error, setError] = useState(false);
+    return (
+        <div className="flex flex-col justify-center text-center m-auto drop-shadow-lg rounded-xl border-2 border-skin-stroke-light bg-skin-bg-dark h-1/2 w-1/4">
+            <Logo />
+            <div className="flex justify-around">
+                <Button label="new" setBasePage={setBasePage} setFunction={setHomepage}/>
+                <Button setBasePage={setBasePage} label="open" setName={setName} setPath={setPath} setLanguage={setLanguage}/>
+            </div>
+        </div>
+    );
+}
 
 export default CenterCube;
